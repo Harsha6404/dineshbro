17867eb3d5929a564896a4b9bf45c7a28f999fe1
Harsha6404 <151925032+Harsha6404@users.noreply.github.com>
2026-01-31 21:15:38 +0530

    Update main.tf

--- hotel-devsecops/terraform/envs/dev/main.tf
+++ hotel-devsecops/terraform/envs/dev/main.tf
@@ -4,3 +4,24 @@ module "vpc" {
   cidr = "10.0.0.0/16"
   name = "hotel-vpc"
 }
+
+module "subnets" {
+  source = "../../modules/subnet"
+
+  vpc_id = module.vpc.vpc_id
+
+  public_cidrs = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
+
+  private_cidrs = [
+    "10.0.101.0/24",
+    "10.0.102.0/24"
+  ]
+
+  azs = [
+    "ap-south-1a",
+    "ap-south-1b"
+  ]
+}
