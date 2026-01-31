8edd190c883bfff3a15a425dbaa0a0ffbe5fe9ce
Harsha6404 <151925032+Harsha6404@users.noreply.github.com>
2026-01-31 21:31:31 +0530

    Create main.tf

---
+++ hotel-devsecops/terraform/modules/network/main.tf
@@ -0,0 +1,62 @@
+resource "aws_internet_gateway" "this" {
+  vpc_id = var.vpc_id
+
+  tags = {
+    Name = "hotel-igw"
+  }
+}
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "this" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = var.public_subnet_ids[0]
+
+  tags = {
+    Name = "hotel-nat"
+  }
+
+  depends_on = [aws_internet_gateway.this]
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = var.vpc_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = {
+    Name = "hotel-public-rt"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = var.vpc_id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.this.id
+  }
+
+  tags = {
+    Name = "hotel-private-rt"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnet_ids)
+
+  subnet_id      = var.public_subnet_ids[count.index]
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnet_ids)
+
+  subnet_id      = var.private_subnet_ids[count.index]
+  route_table_id = aws_route_table.private.id
+}
